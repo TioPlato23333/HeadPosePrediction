de87d88b9ca60440e2405c1b48c97112764df480
TioPlato <ningxiaodong23333@gmail.com>
2023-09-17 19:42:36 +0800

    Extract middle layers of resnet.

--- resnet_test.py
+++ resnet_test.py
@@ -1,6 +1,7 @@
 import PIL 
 import matplotlib.pyplot as plt 
 import numpy as np 
+import tensorflow as tf
 from keras.applications.imagenet_utils import decode_predictions
 from tensorflow.keras.utils import load_img
 from tensorflow.keras.utils import img_to_array 
@@ -11,8 +12,8 @@ if __name__ == '__main__':
     ## load an image in PIL format 
     original = load_img(filename, target_size=(224, 224)) 
     print('PIL image size', original.size)
-    plt.imshow(original) 
-    plt.show()
+    # plt.imshow(original) 
+    # plt.show()
     # convert the PIL image to a numpy array 
     numpy_image = img_to_array(original) 
     plt.imshow(np.uint8(numpy_image)) 
@@ -30,3 +31,9 @@ if __name__ == '__main__':
     # convert the probabilities to class labels 
     label = decode_predictions(predictions)
     print(label)
+    # extract middle layer output 
+    feature_extractor = tf.keras.Model(inputs=resnet_model.inputs, \
+        outputs=[layer.output for layer in resnet_model.layers])
+    features = feature_extractor(processed_image)
+    for i in range(len(features)):
+        print(features[i].shape)
