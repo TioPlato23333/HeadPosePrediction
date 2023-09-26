a12c021062ac9bdfdb1b7a7b3110d0bce214bb0e
TioPlato <ningxiaodong23333@gmail.com>
2023-09-26 20:46:48 +0800

    Test conv layers.

--- layer_test.py
+++ layer_test.py
@@ -3,11 +3,13 @@ import tensorflow as tf
 
 if __name__ == '__main__':
     # architecture
-    inputs = tf.keras.Input(shape=(10, 10))
-    outputs = tf.keras.layers.Dense(1)(inputs)
+    inputs = tf.keras.Input(shape=(10, 10, 1))
+    outputs = tf.keras.layers.Conv2D(1, 3, input_shape=(10, 10, 1))(inputs)
+    # outputs = tf.keras.layers.Dense(1)(inputs)
     # model
     model = tf.keras.Model(inputs, outputs)
     # test
-    image = np.ones((1, 10, 10))
-    print(image)
-    print(model.predict(image))
+    image = np.ones((1, 10, 10, 1))
+    for layer in model.layers:
+        print(layer.output)
+    # print(model.predict(image))
