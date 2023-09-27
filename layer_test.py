9b6210542eb632c9fa081a8927682cebeb15a41c
TioPlato <ningxiaodong23333@gmail.com>
2023-09-27 22:06:29 +0800

    Test max pooling layer.

--- layer_test.py
+++ layer_test.py
@@ -4,12 +4,17 @@ import tensorflow as tf
 if __name__ == '__main__':
     # architecture
     inputs = tf.keras.Input(shape=(10, 10, 1))
-    outputs = tf.keras.layers.Conv2D(1, 3, input_shape=(10, 10, 1))(inputs)
+    # outputs = tf.keras.layers.Conv2D(1, 3, input_shape=(10, 10, 1))(inputs)
     # outputs = tf.keras.layers.Dense(1)(inputs)
+    outputs = tf.keras.layers.MaxPooling2D()(inputs)
     # model
     model = tf.keras.Model(inputs, outputs)
-    # test
-    image = np.ones((1, 10, 10, 1))
+    print('[INFO] Layer architecture:')
     for layer in model.layers:
         print(layer.output)
-    # print(model.predict(image))
+    # test
+    # image = np.ones((1, 10, 10, 1))
+    image = tf.random.uniform((1, 10, 10, 1))
+    print('[INFO] Input/output:')
+    print(image)
+    print(model.predict(image))
